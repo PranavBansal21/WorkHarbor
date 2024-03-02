1c9cd2ebe528c9d4e8996e5f224646265c33a65f
Pranav <pranav123bansal@gmail.com>
2024-03-02 10:14:49 +0530

    Logout route

---
+++ app/api/users/logout/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logged Out",
+      success: true,
+    });
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    return response;
+  } catch (err) {
+    return NextResponse.json({ error: err.message, status: 500 });
+  }
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { NextResponse } from "next/server";
+
 export default function Home() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error) {
+      return NextResponse.json({ error: error.message, status: 500 });
+    }
+  };
   return (
     <>
+      <button onClick={logout}>LOGOUT</button>
     </>
   );
 }
